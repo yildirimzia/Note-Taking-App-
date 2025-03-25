bed10fe2eeb633933c6a194246cf0157df4b8bdb
ziya yildirim <zia.yildirimm@gmail.com>
2025-03-25 14:51:52 +0300

    Add authentication and user management features with Supabase integration

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -12,7 +12,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export function DarkModelToggle() {
+export default function DarkModeToggle() {
   const { setTheme } = useTheme()
 
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,60 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { shadow } from '@/styles/utils'
-import { Button } from './ui/button'
-import {DarkModelToggle} from './DarkModelToggle'
+import { shadow } from "@/styles/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import DarkModeToggle from "./DarkModeToggle";
+import LogoutButton from "./LogoutButton";
+import { getUser } from "@/auth/server";
+// import { SidebarTrigger } from "./ui/sidebar";
 
-function Header() {
-  const user = null;
+async function Header() {
+  const user = await getUser();
+
+   console.log(user,'user')
+
   return (
-    <header 
-      className='relative flex h-24 w-full items-center justify-between px-3 sm:px-8'
-      style={{boxShadow: shadow}}
+    <header
+      className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-8"
+      style={{
+        boxShadow: shadow,
+      }}
     >
-      <Link href="/" className='flex items-end gap-2'>
-        <Image src="/goatius.png" height={60} width={60} alt='logo' className='rounded-full' priority/>
-        <h1 className='flex flex-col pb-1 text-2xl font-semibold leading-6'>GOAT <span>notes</span></h1>
+      {/* <SidebarTrigger className="absolute left-1 top-1" /> */}
+
+      <Link className="flex items-end gap-2" href="/">
+        <Image
+          src="/goatius.png"
+          height={60}
+          width={60}
+          alt="logo"
+          className="rounded-full"
+          priority
+        />
+
+        <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">
+          GOAT <span>Notes</span>
+        </h1>
       </Link>
 
-      <div className='flex gap-4'>
+      <div className="flex gap-4">
         {user ? (
-          "Logout"
+          <LogoutButton />
         ) : (
-          <>  
+          <>
             <Button asChild>
-              <Link href="/sign-up" className='hidden sm:block'>Login</Link>
+              <Link href="/sign-up" className="hidden sm:block">
+                Sign Up
+              </Link>
             </Button>
-            <Button asChild variant={'outline'}>
-            <Link href="/login">Login</Link>
-          </Button>
+            <Button asChild variant="outline">
+              <Link href="/login">Login</Link>
+            </Button>
           </>
         )}
-        <DarkModelToggle />
+        <DarkModeToggle />
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
